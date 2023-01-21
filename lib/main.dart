d5837716eb92f3b7a9eb7ea9fe23fac912d78707
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-21 23:17:23 +0900

    LoginPageを表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tsumitabe_frontend/src/pages/login_page.dart';
 // import 'package:universal_platform/universal_platform.dart';
-
-import 'src/layout/page_list.dart';
-import 'src/pages/profile_page.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
@@ -19,28 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'プロファイル'),
+      home: const LoginPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        drawer: PageList(),
-        body: ProfilePage());
   }
 }
