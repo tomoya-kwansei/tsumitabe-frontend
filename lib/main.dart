fe032207770d8397233aa27a13ed3e4e9e4a6087
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-24 19:39:24 +0900

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tsumitabe_frontend/src/pages/login_page.dart';
+import 'package:tsumitabe_frontend/src/pages/profile_page.dart';
+import 'package:tsumitabe_frontend/src/view_model/provider.dart';
 // import 'package:universal_platform/universal_platform.dart';
 
 Future main() async {
@@ -14,17 +16,23 @@ Future main() async {
   runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(meProvider);
     return MaterialApp(
       title: 'Tsumitabe',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const LoginPage(),
+      home: value.when(
+          data: (data) {
+            return ProfilePage();
+          },
+          error: (error, _) => LoginPage(),
+          loading: () => const CircularProgressIndicator()),
     );
   }
 }
