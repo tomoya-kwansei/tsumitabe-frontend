4fe5d12a01751105b2b8e58d94434111853c41b9
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-28 21:07:49 +0900

    add provider

--- lib/src/view_model/provider.dart
+++ lib/src/view_model/provider.dart
@@ -20,3 +20,13 @@ final getSharedPreferencesProvider =
     FutureProvider<SharedPreferences>((ref) async {
   return await SharedPreferences.getInstance();
 });
+
+final dashboardProvider = FutureProvider<User>((ref) async {
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString("token");
+  if (token == null) {
+    throw NullThrownError();
+  }
+  final repository = ref.read(authenticateRepository);
+  return await repository.me(token);
+});
