62d28fc573153a0e336eaa02d7124c146848f384
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-29 15:45:59 +0900

    add DashboardComponent

---
+++ lib/src/components/dashboard_page/dashboard_component.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tsumitabe_frontend/src/components/dashboard_page/food_list_component.dart';
+import 'package:tsumitabe_frontend/src/models/food.dart';
+import 'package:tsumitabe_frontend/src/models/user.dart';
+
+class DashboardComponent extends StatefulWidget {
+  final User user;
+  const DashboardComponent({Key? key, required this.user}) : super(key: key);
+
+  @override
+  _DashboardComponentState createState() => _DashboardComponentState();
+}
+
+class _DashboardComponentState extends State<DashboardComponent> {
+  @override
+  Widget build(BuildContext context) {
+    final foods = List.generate(
+        60,
+        (index) => Food(
+            id: index,
+            name: "food No. $index",
+            url: "https://picsum.photos/seed/$index/320/320"));
+    return Stack(
+      children: [FoodListComponent(foods: foods)],
+    );
+  }
+}
