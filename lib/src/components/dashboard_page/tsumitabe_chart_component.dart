27f2920bce82dfbe44cb7ca6049157ae870e6d7e
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-01-30 15:14:02 +0900

    change chart design

--- lib/src/components/dashboard_page/tsumitabe_chart_component.dart
+++ lib/src/components/dashboard_page/tsumitabe_chart_component.dart
@@ -12,39 +12,62 @@ class TsumitabeChartComponent extends StatefulWidget {
 class _TsumitabeChartComponentState extends State<TsumitabeChartComponent> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            height: 300,
-            child: Padding(
-              padding: const EdgeInsets.all(30),
-              child: BarChart(
-                BarChartData(
-                    borderData: FlBorderData(
-                        border: const Border(
-                      top: BorderSide.none,
-                      right: BorderSide.none,
-                      left: BorderSide(width: 1),
-                      bottom: BorderSide(width: 1),
-                    )),
-                    groupsSpace: 5,
-                    barGroups: [
-                      BarChartGroupData(x: 1, barRods: [
-                        BarChartRodData(toY: 10, width: 15),
-                      ]),
-                      BarChartGroupData(x: 2, barRods: [
-                        BarChartRodData(toY: 9, width: 15),
-                      ]),
-                      BarChartGroupData(x: 3, barRods: [
-                        BarChartRodData(toY: 4, width: 15),
-                      ]),
-                      BarChartGroupData(x: 4, barRods: [
-                        BarChartRodData(toY: 2, width: 15),
-                      ]),
-                      BarChartGroupData(x: 5, barRods: [
-                        BarChartRodData(toY: 13, width: 15),
-                      ]),
-                    ]),
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: BarChart(
+        BarChartData(
+            barTouchData: BarTouchData(
+              enabled: false,
+            ),
+            borderData: FlBorderData(
+              show: false,
+            ),
+            gridData: FlGridData(
+              show: true,
+              checkToShowHorizontalLine: (value) => value % 10 == 0,
+              getDrawingHorizontalLine: (value) => FlLine(
+                strokeWidth: 1,
               ),
-            )));
+              drawVerticalLine: false,
+            ),
+            barGroups: [
+              BarChartGroupData(x: 1, barRods: [
+                BarChartRodData(
+                  toY: 10,
+                  width: 25,
+                  borderRadius: BorderRadius.zero,
+                ),
+              ]),
+              BarChartGroupData(x: 2, barRods: [
+                BarChartRodData(
+                  toY: 9,
+                  width: 25,
+                  borderRadius: BorderRadius.zero,
+                ),
+              ]),
+              BarChartGroupData(x: 3, barRods: [
+                BarChartRodData(
+                  toY: 4,
+                  width: 25,
+                  borderRadius: BorderRadius.zero,
+                ),
+              ]),
+              BarChartGroupData(x: 4, barRods: [
+                BarChartRodData(
+                  toY: 2,
+                  width: 25,
+                  borderRadius: BorderRadius.zero,
+                ),
+              ]),
+              BarChartGroupData(x: 5, barRods: [
+                BarChartRodData(
+                  toY: 13,
+                  width: 25,
+                  borderRadius: BorderRadius.zero,
+                ),
+              ]),
+            ]),
+      ),
+    );
   }
 }
