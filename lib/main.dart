3734e0d2e658f49d455a55abd3bd6e75e0711eb1
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-02-01 13:07:24 +0900

    initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tsumitabe_frontend/src/pages/dashboard_page.dart';
 import 'package:tsumitabe_frontend/src/pages/login_page.dart';
@@ -11,6 +12,19 @@ Future main() async {
     await dotenv.load(fileName: "assets/.env-local");
   } else {
     await dotenv.load(fileName: "assets/.env");
+  }
+
+  final facebookAppId = dotenv.env["FACEBOOK_APP_ID"];
+  if (facebookAppId == null) {
+    throw "環境変数にFACEBOOK_APP_IDがありません";
+  }
+  if (kIsWeb || defaultTargetPlatform == TargetPlatform.macOS) {
+    await FacebookAuth.instance.webAndDesktopInitialize(
+      appId: facebookAppId,
+      cookie: true,
+      xfbml: true,
+      version: "v15.0",
+    );
   }
   runApp(const ProviderScope(child: TsumitabeApp()));
 }
